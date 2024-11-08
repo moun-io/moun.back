1477888fe9f5cccb0a76ff10becd30f9aba9539c
rojae1339 <wjdwogh1224@gmail.com>
2024-11-08 10:10:13 +0100

    add
    Enum Genre, Role

---
+++ moun/src/main/java/io/moun/api/domain/member/Role.java
@@ -0,0 +1,11 @@
+package io.moun.api.domain.member;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER,
+    ADMIN
+}
