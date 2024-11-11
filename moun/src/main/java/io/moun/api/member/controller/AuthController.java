bd23e46dbd248762bc5497cde31c69877d75dc46
Jinhyuk <kobin1970@gmail.com>
2024-11-11 20:28:19 +0100

    set context-path

--- moun/src/main/java/io/moun/api/member/controller/AuthController.java
+++ moun/src/main/java/io/moun/api/member/controller/AuthController.java
@@ -1,9 +1,15 @@
 package io.moun.api.member.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
+    @PostMapping("/register")
+    public ResponseEntity<String> register() {
+        return ResponseEntity.ok("success");
+    }
 }
