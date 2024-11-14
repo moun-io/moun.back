3ebf3010099834f079505b1ee7834da74e9033d1
Jinhyuk <kobin1970@gmail.com>
2024-11-14 22:22:46 +0100

    Error Handling : JWTFilter UsernameNotFound

--- moun/src/main/java/io/moun/api/security/exception/AuthExceptionHandler.java
+++ moun/src/main/java/io/moun/api/security/exception/AuthExceptionHandler.java
@@ -1,35 +1,41 @@
 package io.moun.api.security.exception;
 
-import io.moun.api.security.service.AuthService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.security.sasl.AuthenticationException;
 
 @RestControllerAdvice
 public class AuthExceptionHandler {
 
-    private final Logger LOGGER = LoggerFactory.getLogger(AuthService.class);
+    private final Logger logger = LoggerFactory.getLogger(AuthExceptionHandler.class);
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> authenticationExceptionHandler(final Exception e) {
-        LOGGER.info(e.getMessage());
+        logger.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<String> usernameNotFoundExceptionHandler(final UsernameNotFoundException e) {
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Username Not Found with the JWT token. Check the Database"+ e.getMessage());
     }
     @ExceptionHandler(AuthenticationCredentialsNotFoundException.class)
     public ResponseEntity<String> authenticationCredentialsNotFoundExceptionHandler(final AuthenticationCredentialsNotFoundException e) {
-        LOGGER.info(e.getMessage());
+        logger.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("User Credential Not Found : " +  e.getMessage());
     }
     @ExceptionHandler(UsernameAlreadyExistsException.class)
-    public ResponseEntity<String>UsernameAlreadyExistsExceptionHandler(final UsernameAlreadyExistsException e) {
-        LOGGER.info(e.getMessage());
+    public ResponseEntity<String>usernameAlreadyExistsExceptionHandler(final UsernameAlreadyExistsException e) {
+        logger.info(e.getMessage());
         return ResponseEntity.status(HttpStatus.CONFLICT).body("Username Already Exists"+ e.getMessage());
     }
+
 }
 
--- moun/src/main/java/io/moun/api/security/infrastructure/JwtFilter.java
+++ moun/src/main/java/io/moun/api/security/infrastructure/JwtFilter.java
@@ -1,7 +1,6 @@
 package io.moun.api.security.infrastructure;
 
 import io.moun.api.security.domain.vo.JwtToken;
-import io.moun.api.security.infrastructure.JwtTokenHelper;
 import io.moun.api.security.service.IJwtTokenHelper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -12,6 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -33,15 +33,26 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-//        jwtTokenHelper.setTokenFromRequest(request);
         JwtToken jwtToken = jwtTokenHelper.getJwtToken();
         if(jwtToken !=null && StringUtils.hasText(jwtToken.getValue()) && jwtTokenHelper.isValidToken()) {
-            String username = jwtTokenHelper.getUsername();
-            UserDetails userDetails = userDetailsService.loadUserByUsername(username);
-            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            try{
+                String username = jwtTokenHelper.getUsername();
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                filterChain.doFilter(request, response);
+            } catch (UsernameNotFoundException e) {
+                handleException(response,e,500,"Username Not Found Check the Database");
+            } catch (Exception e){
+                handleException(response,e,500,"An error occurred while logging in");
+            }
+
         }
-        filterChain.doFilter(request, response);
+
+    }
+    private void handleException(HttpServletResponse response, Exception e, int statusCode, String msg) throws IOException {
+        response.setStatus(statusCode);
+        response.getWriter().write("Filter Error: " + msg + "\n" + e +  e.getMessage());
     }
 }
